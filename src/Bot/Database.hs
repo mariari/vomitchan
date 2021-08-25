11e04fd54eda8b8b297f71372d99fdc620dc9d30
nllptr <nllptr@arisu.world>
2021-08-25 19:56:53 -0400

    Clean up of queries

--- src/Bot/Database.hs
+++ src/Bot/Database.hs
@@ -49,40 +49,92 @@ ifEmpty [] def _  = def
 
 addUser :: Username -> Channel -> IO ()
 addUser user chan = withConnection "./data/vomits.db" $
-  \conn -> executeNamed conn "INSERT INTO user (username, channel_id, quantity_of_vomits) VALUES (:uname, (SELECT id FROM channels WHERE name=:cname), 0)" [":uname" := user, ":cname" := chan]
+  \conn ->
+    executeNamed
+        conn
+        "INSERT INTO user (username, channel_id, quantity_of_vomits)\
+        \ VALUES (:uname, (SELECT id FROM channels WHERE name=:cname), 0)"
+        [":uname" := user, ":cname" := chan]
 
 addVomit :: Username -> Channel -> String -> String -> IO ()
 addVomit nick chan md5 filepath = withConnection "./data/vomits.db" $
-  \conn -> executeNamed conn "INSERT INTO vomits (filepath, vomit_md5, user_id) VALUES (:filepath, :md5, (SELECT id FROM user WHERE username=:uname AND channel_id=(SELECT id FROM channels where name=:cname)))" [":filepath" := filepath, ":md5" := md5, ":uname" := nick, ":cname" := chan]
+  \conn ->
+    executeNamed
+        conn
+        "INSERT INTO vomits (filepath, vomit_md5, user_id)\
+        \ VALUES (:filepath, :md5, (SELECT id FROM user WHERE username=:uname\
+        \ AND channel_id=(SELECT id FROM channels where name=:cname)))"
+        [":filepath" := filepath, ":md5" := md5, ":uname" := nick, ":cname" := chan]
 
 updateLink :: String -> String -> IO ()
 updateLink filepath link = withConnection "./data/vomits.db" $
-  \conn -> executeNamed conn "UPDATE vomits SET link=:link WHERE filepath=:path" [":link" := link, ":path" := filepath]
+  \conn ->
+    executeNamed
+        conn
+        "UPDATE vomits SET link=:link WHERE filepath=:path"
+        [":link" := link, ":path" := filepath]
 
 succUserQuantityOfVomits :: Username -> Channel -> IO ()
 succUserQuantityOfVomits user chan = withConnection "./data/vomits.db" $
-  \conn -> executeNamed conn "UPDATE user SET quantity_of_vomits = quantity_of_vomits + 1 WHERE username=:uname AND channel_id=(SELECT id FROM channels WHERE name=:cname)" [":uname" := user, ":cname" := chan]
+  \conn ->
+    executeNamed
+        conn
+        "UPDATE user SET quantity_of_vomits = quantity_of_vomits + 1\
+        \ WHERE username=:uname\
+        \ AND channel_id=(SELECT id FROM channels WHERE name=:cname)"
+        [":uname" := user, ":cname" := chan]
 
 updateUserQuantityOfVomits :: Username -> Channel -> Int -> IO ()
 updateUserQuantityOfVomits user chan new = withConnection "./data/vomits.db" $
-  \conn -> executeNamed conn "UPDATE user SET quantity_of_vomits=:voms WHERE username=:uname AND channel_id=(SELECT id FROM channels WHERE name=:cname);" [":voms" := new, ":uname" := user, ":cname" := chan]
+  \conn ->
+    executeNamed
+        conn
+        "UPDATE user SET quantity_of_vomits=:voms\
+        \ WHERE username=:uname\
+        \ AND channel_id=(SELECT id FROM channels WHERE name=:cname);"
+        [":voms" := new, ":uname" := user, ":cname" := chan]
 
 getUserQuantityOfVomits :: Username -> Channel -> IO Int
-getUserQuantityOfVomits username chan = withConnection "./data/vomits.db" $ \conn -> do
-  user <- queryNamed conn "SELECT * FROM user WHERE username=:uname AND channel_id=(SELECT id FROM channels WHERE name=:cname) LIMIT 1;" [":uname" := username, ":cname" := chan] :: IO [DBUser]
-  return $ ifEmpty user 0 userQuantityVomit
+getUserQuantityOfVomits username chan = withConnection "./data/vomits.db" $
+  \conn -> do
+    user <- queryNamed
+                conn
+                "SELECT * FROM user WHERE username=:uname\
+                \ AND channel_id=(SELECT id FROM channels WHERE name=:cname)\
+                \ LIMIT 1;"
+                [":uname" := username, ":cname" := chan] :: IO [DBUser]
+    return $ ifEmpty user 0 userQuantityVomit
 
 getLink :: String -> IO (Maybe String)
-getLink filepath = withConnection "./data/vomits.db" $ \conn -> do
-  vom <- queryNamed conn "SELECT * FROM vomits WHERE filepath=:path LIMIT 1;" [":path" := filepath] :: IO [DBVomit]
-  return $ ifEmpty vom Nothing vomitLink
+getLink filepath = withConnection "./data/vomits.db" $
+  \conn -> do
+    vom <- queryNamed
+                conn
+                "SELECT * FROM vomits WHERE filepath=:path LIMIT 1;"
+                [":path" := filepath] :: IO [DBVomit]
+    return $ ifEmpty vom Nothing vomitLink
 
 getRandomVomitPath :: Username-> Channel-> IO String
-getRandomVomitPath user chan = withConnection "./data/vomits.db" $ \conn -> do
-  vom  <- queryNamed conn "SELECT * FROM vomits WHERE user_id=(SELECT id FROM user WHERE username=:uname AND channel_id=(SELECT id FROM channels WHERE name=:cname)) ORDER BY RANDOM() LIMIT 1;" [":uname" := user, ":cname" := chan] :: IO [DBVomit]
-  return $ ifEmpty vom "" vomitPath
+getRandomVomitPath user chan = withConnection "./data/vomits.db" $
+  \conn -> do
+    vom  <- queryNamed
+                conn
+                "SELECT * FROM vomits\
+                \ WHERE user_id=(SELECT id FROM user WHERE username=:uname\
+                \ AND channel_id=(SELECT id FROM channels WHERE name=:cname))\
+                \ ORDER BY RANDOM() LIMIT 1;"
+                [":uname" := user, ":cname" := chan] :: IO [DBVomit]
+    return $ ifEmpty vom "" vomitPath
 
 getRouletteVomit :: Channel -> IO String
-getRouletteVomit chan = withConnection "./data/vomits.db" $ \conn -> do
-  vom  <- queryNamed conn "SELECT * FROM vomits WHERE user_id=(SELECT id FROM user WHERE channel_id=(SELECT id FROM channels WHERE name=:cname) AND quantity_of_vomits>=1 ORDER BY RANDOM() LIMIT 1) ORDER BY RANDOM() LIMIT 1;" [":cname" := chan] :: IO [DBVomit]
-  return $ ifEmpty vom "" vomitPath
+getRouletteVomit chan = withConnection "./data/vomits.db" $
+  \conn -> do
+    vom  <- queryNamed
+                conn
+                "SELECT * FROM vomits\
+                \ WHERE user_id=(SELECT id FROM user\
+                \ WHERE channel_id=(SELECT id FROM channels WHERE name=:cname)\
+                \ AND quantity_of_vomits>=1 ORDER BY RANDOM() LIMIT 1)\
+                \ ORDER BY RANDOM() LIMIT 1;"
+                [":cname" := chan] :: IO [DBVomit]
+    return $ ifEmpty vom "" vomitPath
